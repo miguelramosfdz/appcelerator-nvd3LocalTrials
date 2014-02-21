47b8ef2f96ee8ab1e7cfefbf208860b62ec2fff3
Justin Aschenbener <jaschenbener@proteusdh.com>
2014-02-20 16:44:47 -0800

    Finally loading an html page locally. Appcelerators worthless filestructure where it deletes stuff placed in Resources and retains stuff in assets probably deleted at least half an hour from my life

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,18 @@
+function loadVisualization() {
+	console.log("~~~ loading ~~~");
+	
+	var newFile = Titanium.Filesystem.getFile('d3_Resources/ex_line_chart.html');
+	// var newFile = Titanium.Filesystem.getFile('d3_Resources/hello.html');
+	// var newFile = Titanium.Filesystem.getFile('hello.html');
+	
+	var visualizationPath = newFile.nativePath;
+	
+	$.visualizerWebView.url = visualizationPath;
+}
+
 // view lifecycles
 function windowPostlayout(e) {
-	$.visualizerWebView.url = "http://www.google.com";
+	loadVisualization();
 }
 
 $.index.open();
